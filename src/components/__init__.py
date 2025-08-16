2402e1854cf69a8b196faf68d28873a020e59a0c
Logan Boehm <sidwirb@gmail.com>
2025-08-15 22:33:09 -0400

    Migrated preview overlay

--- src/components/__init__.py
+++ src/components/__init__.py
@@ -6,3 +6,4 @@ from .model import MeshModel
 from .grid_background import GridBackground
 from .editor_view import EditorView
 from .preview_widget import PreviewWidget
+from .preview_overlay import PreviewOverlay
---
+++ src/components/preview_overlay.py
@@ -0,0 +1,54 @@
+from PySide6 import QtCore, QtGui, QtWidgets
+
+class PreviewOverlay(QtWidgets.QGraphicsItem):
+    """Draws live helpers: add-vertex ghost + triangle-mode previews."""
+    def __init__(self, main_ref):
+        super().__init__()
+        self.setZValue(9999)  # above everything
+        self.setAcceptedMouseButtons(QtCore.Qt.NoButton)
+        self._main = main_ref
+        self._mouse = QtCore.QPointF()
+
+        # reusable pens/brush
+        self._helper_pen = QtGui.QPen(QtGui.QColor(255, 255, 255, 160), 5)
+        self._helper_pen.setCosmetic(True)
+        self._ghost_pen = QtGui.QPen(QtGui.QColor(255, 255, 255, 180), 5)
+        self._ghost_pen.setCosmetic(True)
+        self._ghost_br = QtGui.QBrush(QtGui.QColor(0, 0, 0, 30))
+
+    def setMouse(self, pt: QtCore.QPointF):
+        if pt == self._mouse:
+            return
+        self.prepareGeometryChange()
+        self._mouse = pt
+        self.update()
+
+    # gigantic rect so we always get repaints when needed
+    def boundingRect(self):
+        return QtCore.QRectF(-1e9, -1e9, 2e9, 2e9)
+
+    def paint(self, p, opt, w):
+        m = self._main
+        # 1) Add-vertex ghost
+        if m.adding_vertex:
+            r = 6
+            p.setPen(self._ghost_pen)
+            p.setBrush(self._ghost_br)
+            p.drawEllipse(self._mouse, r, r)
+
+        # 2) Triangle-mode previews
+        if m.tri_mode:
+            buf = m.tri_buffer
+            model = m.models[m.active_mesh]
+            pts = model.points()
+            p.setPen(self._helper_pen)
+            p.setBrush(QtGui.QColor(0, 0, 0, 25))
+
+            if len(buf) == 1 and buf[0] < len(pts):
+                p1 = pts[buf[0]]
+                p.drawLine(p1, self._mouse)
+
+            elif len(buf) == 2 and max(buf) < len(pts):
+                p1, p2 = pts[buf[0]], pts[buf[1]]
+                poly = QtGui.QPolygonF([p1, p2, self._mouse])
+                p.drawPolygon(poly)
