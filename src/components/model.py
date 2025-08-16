156aa062171b9c96a6fe6db1af40a87fb9528198
Logan Boehm <sidwirb@gmail.com>
2025-08-15 22:23:13 -0400

    Migrated model

---
+++ src/components/model.py
@@ -0,0 +1,40 @@
+from PySide6 import QtCore, QtWidgets
+
+class MeshModel(QtCore.QObject):
+    changed = QtCore.Signal()
+
+    def __init__(self):
+        super().__init__()
+        self._pts: list[QtCore.QPointF] = []
+        self._tris: list[tuple[int, int, int]] = []
+
+    # vertices
+    def add_point(self, p: QtCore.QPointF) -> int:
+        self._pts.append(QtCore.QPointF(p))
+        self.changed.emit()
+        return len(self._pts) - 1
+
+    def points(self):
+        return self._pts
+
+    def set_point(self, i: int, p: QtCore.QPointF):
+        self._pts[i] = QtCore.QPointF(p)
+        self.changed.emit()
+
+    # triangles (indices into points)
+    def triangles(self):
+        return self._tris
+
+    def add_triangle(self, i, j, k):
+        if len({i, j, k}) != 3:
+            return
+        n = len(self._pts)
+        if any(idx < 0 or idx >= n for idx in (i, j, k)):
+            return
+        self._tris.append((i, j, k))
+        self.changed.emit()
+
+    def delete_last_triangle(self):
+        if self._tris:
+            self._tris.pop()
+            self.changed.emit()
