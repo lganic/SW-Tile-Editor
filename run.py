b8fc887cdf2f796765610f80b870987abfd29f25
Logan Boehm <sidwirb@gmail.com>
2025-08-15 22:40:45 -0400

    Added run script

---
+++ run.py
@@ -0,0 +1,9 @@
+from PySide6 import QtWidgets
+from src import Main
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    w = Main()
+    w.resize(1200, 650)
+    w.show()
+    app.exec()
